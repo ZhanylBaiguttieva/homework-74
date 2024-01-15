c8c4cb21af7a63b18621184349b5d3e9df11f61e
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-15 12:38:37 +0600

    Added functions getItems, addItem

---
+++ fileDb.ts
@@ -0,0 +1,28 @@
+import {promises as fs} from 'fs';
+import crypto from 'crypto';
+import {Message} from "./types";
+import messages from "./routers/messages";
+const path = 'messages';
+
+const fileDb = {
+    async getItems() {
+        const files = await fs.readdir(path);
+        const filesDataPromises  = files.slice(0,5).map(async (file) => {
+            const filePath = path + '/' + file;
+            const result = await fs.readFile(filePath);
+            return JSON.parse(result.toString());
+        });
+        return await Promise.all(filesDataPromises);
+    },
+    async addItem(item: Message, fileName: string) {
+        const date = new Date;
+        const datetime = date.toISOString();
+
+        const message = {datetime, ...item};
+
+        await fs.writeFile(fileName, JSON.stringify(message));
+        return message;
+    },
+};
+
+export default fileDb;
