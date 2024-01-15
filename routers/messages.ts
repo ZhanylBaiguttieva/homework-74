d1f9aaa55b23d21fde464e1bd7f09530fab33fe6
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-15 12:39:13 +0600

    Added messagesRouter.get and .post

--- routers/messages.ts
+++ routers/messages.ts
@@ -1,16 +1,22 @@
 import crypto from 'crypto';
 import {Router} from 'express';
+import fileDb from "../fileDb";
 const messagesRouter = Router();
-messagesRouter.get('/',  (req, res)=>{
-    res.send();
+messagesRouter.get('/',  async (req, res)=>{
+    const messages = await fileDb.getItems();
+    res.send(messages);
 });
 
-messagesRouter.get('/:id', (req, res)=>{
-    res.send();
-});
+messagesRouter.post('/', async (req, res)=>{
+    const message = {
+        message: req.body.message,
+    };
 
-messagesRouter.post('/', (req, res)=>{
-    res.send();
+    const date = new Date;
+    const datetime = date.toISOString();
+
+    const newMessage = await fileDb.addItem(message, 'messages/' + datetime + '.txt');
+    res.send(newMessage);
 });
 
 export default messagesRouter;
